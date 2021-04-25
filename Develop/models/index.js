038e8934b38b5d919d6f847bbe0fa99c2740361d
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-24 17:35:09 -0700

    Seeds and Models are working

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignkey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignkey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignkey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignkey: 'tag_id',
+});
 
 module.exports = {
   Product,
